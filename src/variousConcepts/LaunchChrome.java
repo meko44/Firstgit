56656f44c0768c316f77364893e030b58f77a047
meko44 <99604174+meko44@users.noreply.github.com>
2022-06-26 09:23:00 -0500

    first commit

---
+++ src/variousConcepts/LaunchChrome.java
@@ -0,0 +1,27 @@
+package variousConcepts;
+
+import java.util.Scanner;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\mcwhy\\FEB 2022-Selenium\\First Selenium\\driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");	
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		driver.close();
+		
+	
+		
+		
+		
+		
+		
+	}
+
+}
